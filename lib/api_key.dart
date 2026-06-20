243c775ebe8188bb30da93bb3178899672a5e11a
DEVA PRASANTH <devaprasanth2706@gmail.com>
2026-06-20 21:19:11 +0530

    Update api_key.dart

--- lib/api_key.dart
+++ lib/api_key.dart
@@ -1,5 +1,5 @@
 class ApiKey {
-  static String key = "6d18068ae628beb79897a43cde96aa91";
+  static String key = "YOUR_API_KEY";
   static String baseUrl = "https://api.themoviedb.org/3";
   static String imageUrl = "https://image.tmdb.org/t/p/w500";
 }
